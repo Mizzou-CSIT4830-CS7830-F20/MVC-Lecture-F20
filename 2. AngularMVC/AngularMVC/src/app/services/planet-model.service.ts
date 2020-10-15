10f1a7c44082cc34ec2ba41cfacbb67d0ad6f1c4
Professor Wergeles <wergelesn@missouri.edu>
2020-10-15 17:14:28 -0500

    adding local storage to have persistent client-side data storage

--- 2. AngularMVC/AngularMVC/src/app/services/planet-model.service.ts
+++ 2. AngularMVC/AngularMVC/src/app/services/planet-model.service.ts
@@ -9,15 +9,33 @@ import { Planets } from 'src/app/types/planets';
 export class PlanetModelService {
   data: Array<Planets> = planets;
 
-  constructor() {}
+  constructor() {
+    this.data =
+      localStorage.getItem('planets') !== null
+        ? JSON.parse(localStorage.getItem('planets'))
+        : planets;
+  }
 
   addData(formObject: Planets) {
+    this.data =
+      localStorage.getItem('planets') !== null
+        ? JSON.parse(localStorage.getItem('planets'))
+        : planets;
+
     this.data.push(formObject);
+
+    localStorage.setItem('planets', JSON.stringify(this.data));
+
     console.log(this.data);
     return this.data;
   }
 
   getData() {
+    this.data =
+      localStorage.getItem('planets') !== null
+        ? JSON.parse(localStorage.getItem('planets'))
+        : planets;
+
     return this.data;
   }
 
